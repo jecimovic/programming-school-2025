3dc2fc58079704b1d2f56d35e6f0cfa3424caf61
Julia Ečimovič <62515099+jecimovic@users.noreply.github.com>
2025-03-27 09:36:35 -0400

    wow

--- julia/app/src/main/java/org/thing.java
+++ julia/app/src/main/java/org/thing.java
@@ -1,11 +1,26 @@
 package org;
 
+import java.util.Random;
+
 public class thing {
+    
+    static void printarray(int[] array){
+        for(int geeeyat = 0; geeeyat<array.length; geeeyat++){
+            System.out.println(array[geeeyat]);
+        }
+    }
 
     public static void main(String[] args) {
+
+        
+
+        Random rnd = new Random();
+
+
         int[] array = new int[50];
         array[0]=1;
         int aaaaaaaaa = 1;
+        int[] araray = new int[10];
         System.out.println(array[0]);
 
         for(int i = 1; i<=49; i++){
@@ -20,7 +35,28 @@ public class thing {
         for (int x = 0; x<49; x++){
             System.out.println(array[x]-avg);
         }
+
+        int[] arrarray = new int[10];
+
+        for (int e = 0; e<10; e++){
+            araray[e]= rnd.nextInt(20);
+        }
         
+        printarray(araray);
+
+        System.out.println("el blocker mi amigos yayaya");
+        
+        int geyat = 0;
+
+        for (int sigma = 9; sigma>-1; sigma--){
+            arrarray[sigma] = araray[geyat];
+            geyat ++;
+        }
+
+        printarray(arrarray);
+        
+        
+
     }
 
 }
